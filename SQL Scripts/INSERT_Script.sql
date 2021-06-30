c1cf183692402d794ec171974dd8326683ca73cb
Aidan Carrigy <aidan.carr2014@gmail.com>
2021-06-30 17:21:06 +0100

    SQL Scripts and Documentation

---
+++ SQL Scripts/INSERT_Script.sql
@@ -0,0 +1,21 @@
+/*SHOW DATABASES; 
+CREATE database CARHIRE; 
+CREATE table CARS(ID int, Make varchar(255), Model varchar(255), Manual Boolean, Year date, Colour varchar(255)); 
+ INSERT INTO CARS values(0, 'Volvo', 'S90', True, '2021-01-01', 'Silver');   
+ INSERT INTO CARS values(1, 'BMW', '520', True, '2020-01-01', 'Black');  
+ INSERT INTO CARS values(2, 'Range Rover', 'Sport', True, '2019-01-01', 'Navy'); 
+ INSERT INTO CARS values(3, 'Mercedes', 'Cla', True, '2021-01-01', 'White'); 
+ INSERT INTO CARS values(4, 'BMW', 'X5', True, '2021-01-01', 'Red'); 
+ INSERT INTO CARS values(5, 'Ford', 'Mondeo', True, '2018-01-01', 'Blue'); */  
+ SELECT * FROM CARS; 
+
+ 
+ 
+ 
+ 
+ 
+  
+
+ 
+
+
